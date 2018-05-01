9c2b13c091f35882e68adfbdb88143999da498bd
febert <frederik.ebert@mytum.de>
2018-05-01 00:06:19 -0700

    sc

--- runscript.py
+++ runscript.py
@@ -10,4 +10,4 @@ for g in ngpus:
     cmd = 'CUDA_VISIBLE_DEVICES={} python scripts/train.py --input_dir /mnt/pushing_data/cartgripper_updown_sact/train --dataset cartgripper --model savp --model_hparams_dict hparams/bair/ours_deterministic_l1/model_hparams.json --model_hparams tv_weight=0.001,transformation=flow,last_frames=2,generate_scratch_image=false,batch_size=16 --summary_freq 10 --timing_file time{}.txt'.format(gpustr, gpustr)
 
     print(cmd)
-    # os.system(cmd)
+    os.system(cmd)
