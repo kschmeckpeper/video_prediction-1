8a9f7cc64438d0c57bab79c5a2d1fa3ed786d333
febert <frederik.ebert@mytum.de>
2018-07-30 20:16:55 -0700

    added sawyer_sim_dataset

---
+++ video_prediction/datasets/sawyer_sim_dataset.py
@@ -0,0 +1,27 @@
+import itertools
+
+from .base_dataset import VideoDataset
+from .softmotion_dataset import SoftmotionVideoDataset
+
+
+class SawyerSimVideoDataset(SoftmotionVideoDataset):
+    def __init__(self, *args, **kwargs):
+        VideoDataset.__init__(self, *args, **kwargs)
+        self.state_like_names_and_shapes['images'] = '%d/env/image_view{}/encoded'.format(self.hparams.image_view), (48, 64, 3)
+        if self.hparams.use_state:
+            self.state_like_names_and_shapes['states'] = '%d/env/state', (5,)
+            self.action_like_names_and_shapes['actions'] = '%d/policy/actions', (4,)
+        # self._check_or_infer_shapes()
+
+    def get_default_hparams_dict(self):
+        default_hparams = super(SawyerSimVideoDataset, self).get_default_hparams_dict()
+        hparams = dict(
+            context_frames=2,
+            sequence_length=15,
+            time_shift=3,
+            use_state=True,
+            sdim=5,
+            adim=5,
+            image_view=0
+        )
+        return dict(itertools.chain(default_hparams.items(), hparams.items()))
