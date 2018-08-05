c31047804189f90f4cd91326fbf56fb257105f26
Sudeep Dasari <sdasari@berkeley.edu>
2018-08-05 16:29:43 -0700

    added sawyer robot experiments

--- video_prediction/datasets/sawyer_sim_dataset.py
+++ video_prediction/datasets/sawyer_sim_dataset.py
@@ -9,8 +9,8 @@ class SawyerSimVideoDataset(SoftmotionVideoDataset):
         VideoDataset.__init__(self, *args, **kwargs)
         self.state_like_names_and_shapes['images'] = '%d/env/image_view{}/encoded'.format(self.hparams.image_view), (48, 64, 3)
         if self.hparams.use_state:
-            self.state_like_names_and_shapes['states'] = '%d/env/state', (5,)
-            self.action_like_names_and_shapes['actions'] = '%d/policy/actions', (4,)
+            self.state_like_names_and_shapes['states'] = '%d/env/state', (self.hparams.sdim,)
+            self.action_like_names_and_shapes['actions'] = '%d/policy/actions', (self.hparams.adim,)
         self._check_or_infer_shapes()
 
     def get_default_hparams_dict(self):
@@ -21,7 +21,7 @@ class SawyerSimVideoDataset(SoftmotionVideoDataset):
             time_shift=3,
             use_state=True,
             sdim=5,
-            adim=5,
+            adim=4,
             image_view=0,
             compressed = True
         )
