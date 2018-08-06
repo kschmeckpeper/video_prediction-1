a225f3c841d2f3d083daeb1c1f10b170198653f4
Sudeep Dasari <sdasari@berkeley.edu>
2018-08-06 12:22:24 -0700

    renamed sawyer_sim_dataset to sawyer_dataset

--- video_prediction/datasets/sawyer_dataset.py
+++ video_prediction/datasets/sawyer_dataset.py
@@ -4,7 +4,7 @@ from .base_dataset import VideoDataset
 from .softmotion_dataset import SoftmotionVideoDataset
 
 
-class SawyerSimVideoDataset(SoftmotionVideoDataset):
+class SawyerVideoDataset(SoftmotionVideoDataset):
     def __init__(self, *args, **kwargs):
         VideoDataset.__init__(self, *args, **kwargs)
         self.state_like_names_and_shapes['images'] = '%d/env/image_view{}/encoded'.format(self.hparams.image_view), (48, 64, 3)
@@ -14,7 +14,7 @@ class SawyerSimVideoDataset(SoftmotionVideoDataset):
         self._check_or_infer_shapes()
 
     def get_default_hparams_dict(self):
-        default_hparams = super(SawyerSimVideoDataset, self).get_default_hparams_dict()
+        default_hparams = super(SawyerVideoDataset, self).get_default_hparams_dict()
         hparams = dict(
             context_frames=2,
             sequence_length=15,
