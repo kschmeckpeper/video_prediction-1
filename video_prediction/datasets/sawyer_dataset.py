28100ed282e8bd3e102b14e1561279eb9bc9d034
Sudeep Dasari <sdasari@berkeley.edu>
2018-08-19 18:31:58 -0700

    started testing bowl with new hparams

--- video_prediction/datasets/sawyer_dataset.py
+++ video_prediction/datasets/sawyer_dataset.py
@@ -1,5 +1,5 @@
 import itertools
-
+import tensorflow as tf
 from .base_dataset import VideoDataset
 from .softmotion_dataset import SoftmotionVideoDataset
 
@@ -10,6 +10,8 @@ class SawyerVideoDataset(SoftmotionVideoDataset):
         self.state_like_names_and_shapes['images'] = '%d/env/image_view{}/encoded'.format(self.hparams.image_view), (48, 64, 3)
         if self.hparams.use_state:
             self.state_like_names_and_shapes['states'] = '%d/env/state', (self.hparams.sdim,)
+            if self.hparams.append_touch:
+                self.state_like_names_and_shapes['touch'] = '%d/env/finger_sensors', (1,)
         self.action_like_names_and_shapes['actions'] = '%d/policy/actions', (self.hparams.adim,)
         self._check_or_infer_shapes()
 
@@ -23,6 +25,15 @@ class SawyerVideoDataset(SoftmotionVideoDataset):
             sdim=5,
             adim=4,
             image_view=0,
-            compressed = True
+            compressed = True,
+            append_touch=False
         )
         return dict(itertools.chain(default_hparams.items(), hparams.items()))
+
+    def parser(self, serialized_example):
+        state_like_seqs, action_like_seqs = super(SawyerVideoDataset, self).parser(serialized_example)
+        if self.hparams.append_touch:
+            touch_data = state_like_seqs.pop('touch')
+            state_like_seqs['states'] = tf.concat((state_like_seqs['states'], tf.nn.sigmoid(touch_data)), axis = -1)
+            print(state_like_seqs)
+            print(1 / 0)
