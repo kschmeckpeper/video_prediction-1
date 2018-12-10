cfd6a86de65b0cbed96714d7851d6c589947e0ea
febert <frederik.ebert@mytum.de>
2018-12-09 21:36:55 -0800

    sc

--- video_prediction/hebbian_lstm/mini_lstm_net.py
+++ video_prediction/hebbian_lstm/mini_lstm_net.py
@@ -10,7 +10,7 @@ class CustomCell(tf.nn.rnn_cell.RNNCell):
     def __init__(self, inputs, reuse=None):
         super(CustomCell, self).__init__(_reuse=reuse)
 
-        self._output_size = tf.TensorShape(1)
+        self._output_size = tf.TensorShape(100)
         self._state_size = [tf.TensorShape(100), tf.TensorShape(100)]
 
     def zero_state(self, batch_size, dtype):
@@ -25,14 +25,18 @@ class CustomCell(tf.nn.rnn_cell.RNNCell):
     def state_size(self):
         return self._state_size
 
-    def call(self, inputs, states):
+    def call(self, inputs, state):
 
         ndense = 100
-        dense_out = dense(inputs, ndense)
+        with tf.variable_scope('h1'):
+            h1= dense(inputs, ndense)
 
         simplelstmcell = SimpleLSTMCell(ndense, 1)
 
-        simplelstmcell(dense_out, states)
+        with tf.variable_scope('h2'):
+            h2, state = simplelstmcell(h1, state)
+
+        return dense(h2, 1), state
 
 
 def make_mini_lstm(inputs, hparams=None):
